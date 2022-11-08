dcc3032e4cd3895af94800633ef55f800e6419f6
vgkon <vgkonstantakopoulos@gmail.com>
2022-11-08 19:18:13 +0200

    last edited summer '22

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by v.konstantakopoulos on 19/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
